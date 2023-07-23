cf09c858a1d5f3c27e37c9de7e318cbf6e595a87
Jesse Narvasa <jsnarvasa@gmail.com>
2023-07-23 10:06:56 +0000

    Bug fix in PASTIS data preprocessing due to images with no annotations

--- preprocess/preprocess_data_pastis.py
+++ preprocess/preprocess_data_pastis.py
@@ -1,4 +1,5 @@
 import os
+import copy
 import numpy as np
 
 
@@ -7,6 +8,10 @@ SEMANTIC_LABEL_PATH = '/home/narvjes/data/PASTIS/ANNOTATIONS'
 SAVE_PATH = '/home/narvjes/data/PASTIS/SAMed'
 FILE_LISTS_PATH = '/home/narvjes/repos/SAMed/lists/lists_PASTIS'
 FILE_LISTS_NAME = 'train.txt'
+
+# Source: https://docs.digitalearthafrica.org/en/latest/data_specs/Sentinel-2_Level-2A_specs.html
+MIN_VALUE = 0
+MAX_VALUE = 10000
 
 
 if not os.path.exists(SAVE_PATH):
@@ -17,27 +22,33 @@ if not os.path.exists(FILE_LISTS_PATH):
 
 S2_files = os.listdir(IMAGE_PATH)
 S2_npy_files = [file for file in S2_files if file.endswith('.npy')]
+S2_npy_files_filtered = copy.deepcopy(S2_npy_files)
 
 for npy_file in S2_npy_files:
     print(f'Processing file {npy_file}')
     patch_id = npy_file.replace('S2_', '').replace('.npy', '')
-    S2_image = np.load(os.path.join(IMAGE_PATH, npy_file))
+    
+    # Note that we are getting the first observation, and the first channel here
+    S2_image = np.load(os.path.join(IMAGE_PATH, npy_file))[0,0,:,:]
+    S2_image_normalised = (S2_image - MIN_VALUE)/(MAX_VALUE - MIN_VALUE)
+
     # Note that the reason we are getting the 0th channel
     # is because channel 0 for TARGET files is the semantic labels
     # as shown here: https://github.com/VSainteuf/pastis-benchmark/blob/main/code/dataloader.py
     try:
         S2_semantic_labels = np.load(os.path.join(SEMANTIC_LABEL_PATH, f'TARGET_{patch_id}.npy'))[0]
     except:
+        S2_npy_files_filtered.remove(npy_file)
         continue
 
     np.savez(
         os.path.join(SAVE_PATH, f'S2_{patch_id}.npz'),
         label = S2_semantic_labels,
-        image = S2_image[0,0,:,:], # note that we are getting the first observation, and the first channel here
+        image = S2_image_normalised,
     )
 
 with open(os.path.join(FILE_LISTS_PATH, FILE_LISTS_NAME), 'w') as f:
-    for patch in S2_npy_files:
+    for patch in S2_npy_files_filtered:
         # Removing the .npy suffix from the text file, since just need S2_{patch_id}
         # The train.py script will be responsible for appending the .npz extension to the filenames
         f.write(patch.replace('.npy', '') + '\n')
